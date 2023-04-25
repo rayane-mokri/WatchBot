f8d4af8afb6232a5a7750c6ca9d82742786d4965
rayane-mokri <rayane.mokri@hotmail.fr>
2023-04-25 22:43:47 +0200

    Basic discord bot

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents = intents)
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(TOKEN)
