cd78d46b0904a3b8a35d5f774ed3928b6104279b
rayane-mokri <rayane.mokri@hotmail.fr>
2023-05-28 23:34:00 +0200

    reddit working

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import os
-import random
+import time
 import discord
+
 from discord.ext import commands
 from dotenv import load_dotenv
 
+import reddit
+
+top_articles = []
+
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+REDDIT_ID = os.getenv('REDDIT_ID')
+REDDIT_SECRET = os.getenv('REDDIT_SECRET')
+REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
+REDDIT_PSSWD = os.getenv('REDDIT_PSSWD')
 
+topics = ["MachineLearning", "python"]
+reddit_top_articles = reddit.reddit_retrieve_top(topics, REDDIT_ID, REDDIT_SECRET,
+                                                REDDIT_USERNAME, REDDIT_PSSWD)
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -15,18 +28,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents = intents)
 
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+@bot.command(name='python', help='display top reddit articles')
 async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
+    for article in reddit_top_articles["python"]:
+        resp = f"**{article['title']}** \n Link : {article['url']}"
+        await ctx.send(resp)
+        time.sleep(2)
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
+@bot.command(name='clear', help='this command will clear 15 msgs')
+async def clear(ctx, amount = 15):
+    await ctx.channel.purge(limit=amount)
+
+@bot.command(name='ml', help='display top reddit articles')
+async def nine_nine(ctx):
+    for article in reddit_top_articles["MachineLearning"]:
+        resp = f"**{article['title']}** \n Link : {article['url']}"
+        await ctx.send(resp)
+        time.sleep(2)
 
 bot.run(TOKEN)
---
+++ reddit.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+def reddit_connect(ID, SECRET, USERNAME, PSSWD):
+    auth = requests.auth.HTTPBasicAuth(ID, SECRET)
+    data = {'grant_type': 'password',
+            'username': USERNAME,
+            'password': PSSWD}
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+    TOKEN = res.json()['access_token']
+    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+    return headers
+
+
+def reddit_retrieve_top(topics, REDDIT_ID, REDDIT_SECRET, REDDIT_USERNAME, REDDIT_PSSWD):
+    darticles = {}
+    for topic in topics :
+        articles = []
+    
+        headers = reddit_connect(REDDIT_ID, REDDIT_SECRET, REDDIT_USERNAME,
+                                REDDIT_PSSWD)
+        requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+        res = requests.get(f"https://oauth.reddit.com/r/{topic}/top/?t=day",
+                        headers=headers)
+        for i in range(5):
+            base = res.json()["data"]["children"][i]["data"]
+            article = {"tag": base["link_flair_text"],
+                    "title": base["title"],
+                    "url": base["url"],
+                    "text":base["selftext"],
+                    "link": "https://www.reddit.com" + base["permalink"]
+                    }
+            articles.append(article)
+        darticles.update({topic : articles})
+    return darticles 
