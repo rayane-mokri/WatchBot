a1f973e569ae36b8304da5c8bc10b0f53a270958
rayane-mokri <rayane.mokri@hotmail.fr>
2023-07-09 21:41:41 +0200

    rework + subs add

--- main.py
+++ main.py
@@ -1,50 +1,111 @@
 import os
+import json
 import time
-import logging
 import discord
-
 from discord.ext import commands
 from dotenv import load_dotenv
 
-import reddit
-
-top_articles = []
-
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-REDDIT_ID = os.getenv('REDDIT_ID')
-REDDIT_SECRET = os.getenv('REDDIT_SECRET')
-REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
-REDDIT_PSSWD = os.getenv('REDDIT_PSSWD')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-topics = ["MachineLearning", "python"]
-reddit_top_articles = reddit.reddit_retrieve_top(topics, REDDIT_ID, REDDIT_SECRET,
-                                                REDDIT_USERNAME, REDDIT_PSSWD)
+with open('articles.json') as f:
+    articles = json.load(f)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents = intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command(name='python', help='display top reddit articles')
-async def nine_nine(ctx):
-    for article in reddit_top_articles["python"][0]:
-        resp = f"**{article['title']}** \n Link : {article['link']}"
-        await ctx.send(resp)
-        time.sleep(2)
+
+@bot.command(name="ml", help="Display top weekly MachineLeaning articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["MachineLearning"][next(iter(articles["MachineLearning"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
+
+@bot.command(name="python", help="Display top weekly Python articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["python"][next(iter(articles["python"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
+
+@bot.command(name="coding", help="Display top weekly Coding articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["coding"][next(iter(articles["coding"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
+
+@bot.command(name="rust", help="Display top weekly Rust articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["rust"][next(iter(articles["rust"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
+
+@bot.command(name="cpp", help="Display top weekly C++ articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["cpp"][next(iter(articles["cpp"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
+
+@bot.command(name="go", help="Display top weekly Golang articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["golang"][next(iter(articles["golang"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
+
+@bot.command(name="unreal", help="Display top weekly Unreal Engine articles on Reddit")
+async def python_articles(ctx):
+    try:
+        for article in articles["unrealengine"][next(iter(articles["unrealengine"].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
 
 @bot.command(name='clear', help='this command will clear 15 msgs')
-async def clear(ctx, amount = 15):
+async def clear(ctx, amount=15):
     await ctx.channel.purge(limit=amount)
 
-@bot.command(name='ml', help='display top reddit articles')
-async def nine_nine(ctx):
-    for article in reddit_top_articles["MachineLearning"][0]:
-        resp = f"**{article['title']}** \n Link : {article['link']}"
-        await ctx.send(resp)
-        time.sleep(2)
-        
-bot.run(TOKEN)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        # Envoyer un message d'erreur lorsque la commande n'est pas trouvée
+        await ctx.send("Commande invalide.")
+
+bot.run(DISCORD_TOKEN)
+
--- reddit.py
+++ reddit.py
@@ -2,47 +2,56 @@ import os
 import json
 import datetime
 import requests
+import progressbar
 from dotenv import load_dotenv
 
 
-def reddit_connect(ID, SECRET, USERNAME, PSSWD):
-    auth = requests.auth.HTTPBasicAuth(ID, SECRET)
-    data = {'grant_type': 'password',
-            'username': USERNAME,
-            'password': PSSWD}
-    headers = {'User-Agent': 'MyBot/0.0.1'}
+def reddit_connect(ID, secret, username, psswd):
+    auth = requests.auth.HTTPBasicAuth(ID, secret)
+    data = {"grant_type": "password",
+            "username": username,
+            "password": psswd}
+    headers = {"User-agent": "WatchBot/0.0.1"}
     res = requests.post('https://www.reddit.com/api/v1/access_token',
                         auth=auth, data=data, headers=headers)
-    TOKEN = res.json()['access_token']
-    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+    token = res.json()["access_token"]
+    headers = {**headers, **{'Authorization': f"bearer {token}"}}
+
     return headers
 
 
-def reddit_retrieve_top(topics, REDDIT_ID, REDDIT_SECRET, REDDIT_USERNAME, REDDIT_PSSWD):
+def reddit_retrieve_top(topics, nb_articles=5):
     darticles = {}
-    for topic in topics :
-        articles = []
-    
-        headers = reddit_connect(REDDIT_ID, REDDIT_SECRET, REDDIT_USERNAME,
-                                REDDIT_PSSWD)
-        requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
-        res = requests.get(f"https://oauth.reddit.com/r/{topic}/top/?t=week",
-                        headers=headers)
-        for i in range(5):
-            base = res.json()["data"]["children"][i]["data"]
-            article = {"tag": base["link_flair_text"],
-                    "title": base["title"],
-                    "url": base["url"],
-                    "text":base["selftext"],
-                    "link": "https://www.reddit.com" + base["permalink"],
-                    "date" : str(datetime.date.today())
-                    }
-            articles.append(article)
-        darticles.update({topic : {str(datetime.date.today()) : articles } 
-                          }
-                        )
 
-    with open("reddit.json","w") as fp: 
+    load_dotenv()
+    REDDIT_ID = os.getenv('REDDIT_ID')
+    REDDIT_SECRET = os.getenv('REDDIT_SECRET')
+    REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
+    REDDIT_PSSWD = os.getenv('REDDIT_PSSWD')
+
+    for topic in progressbar.progressbar(topics):
+        articles = []
+        headers = reddit_connect(REDDIT_ID, REDDIT_SECRET, REDDIT_USERNAME, REDDIT_PSSWD)
+        res = requests.get(f"https://oauth.reddit.com/r/{topic}/top/?t=week", headers=headers)
+        for i in range(nb_articles):
+            try:
+                base = res.json()["data"]["children"][i]["data"]
+                article = {"tag": base["link_flair_text"],
+                           "title": base["title"],
+                           "url": base["url"],
+                           "text": base["selftext"],
+                           "link": "https://www.reddit.com" + base["permalink"],
+                           "date": str(datetime.date.today())
+                           }
+                articles.append(article)
+            except:
+                pass
+            darticles.update({topic: {str(datetime.date.today()): articles}})
+
+    with open("articles.json", "w") as fp:
         json.dump(darticles, fp)
-        
-    return darticles 
+
+    return darticles
+
+if __name__ == "__main__":
+    reddit_retrieve_top(["MachineLearning", "coding", "python", "rust", "cpp", "unrealengine", "golang"])
