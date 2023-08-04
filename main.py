dc738ef3dcbf0def7bdce437f514bf8fe902e8f4
rayane-mokri <rayane.mokri@hotmail.fr>
2023-08-04 02:41:17 +0200

    reddit command + cleaner code

--- main.py
+++ main.py
@@ -13,10 +13,22 @@ def filedate(filename):
     return filename.split('_')[1]
 
 
-def return_articles(theme):
-    for article in articles[theme][next(iter(articles[theme].keys()))]:
-        resp = f"**{article['title']}** \n Link : {article['link']}"
-    return resp
+def choose_subject(subject):
+    match subject:
+        case "ml" | "machine learning" | "Machine Learning" | "ML":
+            sub = "MachineLearning"
+        case "go" | "Go" | "golang":
+            sub = "golang"
+        case "Rust":
+            sub = "rust"
+        case "code" | "Code":
+            sub = "coding"
+        case "Unreal" | "unreal" | "unrl":
+            sub = "unrealengine"
+        case _:
+            sub = subject
+
+    return sub
 
 
 dirname = "articles"
@@ -43,81 +55,10 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 
-@bot.command(name="ml", help="Display top weekly MachineLeaning articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["MachineLearning"][next(iter(articles["MachineLearning"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
-
-
-@bot.command(name="python", help="Display top weekly Python articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["python"][next(iter(articles["python"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
-
-
-@bot.command(name="coding", help="Display top weekly Coding articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["coding"][next(iter(articles["coding"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
-
-
-@bot.command(name="rust", help="Display top weekly Rust articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["rust"][next(iter(articles["rust"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
-
-
-@bot.command(name="cpp", help="Display top weekly C++ articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["cpp"][next(iter(articles["cpp"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
-
-
-@bot.command(name="go", help="Display top weekly Golang articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["golang"][next(iter(articles["golang"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
-
-
-@bot.command(name="unreal", help="Display top weekly Unreal Engine articles on Reddit")
-async def python_articles(ctx):
-    try:
-        for article in articles["unrealengine"][next(iter(articles["unrealengine"].keys()))]:
-            resp = f"**{article['title']}** \n Link : {article['link']}"
-            await ctx.send(resp)
-            time.sleep(2)
-    except:
-        await ctx.send("No articles")
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Commande invalide.")
 
 
 @bot.command(name='clear', help='this command will clear 15 msgs')
@@ -125,11 +66,18 @@ async def clear(ctx, amount=100):
     await ctx.channel.purge(limit=amount)
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        # Envoyer un message d'erreur lorsque la commande n'est pas trouvée
-        await ctx.send("Commande invalide.")
+@bot.command(name="reddit", help="Display top 5 weekly. Topic covered : Python, Coding, Rust, Golang, Unreal Engine, C++")
+async def reddit(ctx, subject):
+    sub = choose_subject(subject)
+    try:
+        for article in articles[sub][next(iter(articles[sub].keys()))]:
+            resp = f"**{article['title']}** \n Link : {article['link']}"
+            await ctx.send(resp)
+            time.sleep(2)
+    except:
+        await ctx.send("No articles")
+
 
 bot.run(DISCORD_TOKEN)
+
 
